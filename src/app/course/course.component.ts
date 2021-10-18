9a7e5185dd151f273477a40d80933e9fe53c8b10
guramijibladze <guramijibladze@gmail.com>
2021-10-18 13:22:50 +0400

    added

---
+++ src/app/course/course.component.ts
@@ -0,0 +1,61 @@
+import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {Course} from '../model/course';
+import {
+  map, tap,
+} from 'rxjs/operators';
+import {merge, fromEvent, Observable, concat, throwError, combineLatest} from 'rxjs';
+import {Lesson} from '../model/lesson';
+import { CoursesService } from '../services/courses.service';
+
+
+interface CourseData {
+  course: Course;
+  lessons: Lesson[];
+}
+
+@Component({
+  selector: 'course',
+  templateUrl: './course.component.html',
+  styleUrls: ['./course.component.css']
+})
+export class CourseComponent implements OnInit {
+
+  data$: Observable<CourseData>
+
+  constructor(private route: ActivatedRoute,
+              private coursesService:CoursesService) {
+                
+              }
+
+  ngOnInit() {
+
+    const courseId = parseInt(this.route.snapshot.paramMap.get("courseId"));
+    const course$ = this.coursesService.loadCourseById(courseId);
+    const lessons$ = this.coursesService.loadAllCourseLessons(courseId);
+    this.data$ = combineLatest([course$, lessons$])
+      .pipe(
+        map(([course, lessons]) => {
+          return {
+            course,
+            lessons
+          }
+        }),
+        tap(console.log)
+      )
+
+  }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/app/search-lessons/search-lessons.component.ts
@@ -0,0 +1,62 @@
+import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {Course} from '../model/course';
+import {
+  debounceTime,
+  distinctUntilChanged,
+  startWith,
+  tap,
+  delay,
+  map,
+  concatMap,
+  switchMap,
+  withLatestFrom,
+  concatAll, shareReplay
+} from 'rxjs/operators';
+import {merge, fromEvent, Observable, concat} from 'rxjs';
+import {Lesson} from '../model/lesson';
+import { CoursesService } from '../services/courses.service';
+
+
+@Component({
+  selector: 'course',
+  templateUrl: './search-lessons.component.html',
+  styleUrls: ['./search-lessons.component.css']
+})
+export class SearchLessonsComponent implements OnInit {
+
+  searchResults$: Observable<Lesson[]>
+  activeLesson:Lesson;
+
+  constructor(private courseService:CoursesService) {
+
+  }
+
+  ngOnInit() {
+
+
+  }
+
+  onSerach(search:string){
+    this.searchResults$ = this.courseService.searchLessons(search)
+  }
+
+  openLesson(lesson:Lesson){
+    this.activeLesson = lesson
+  }
+
+  onBackToSerach(){
+    this.activeLesson = null
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
